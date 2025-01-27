b599defdf3c2b41b35e37c2d240606e67ed3dd4c
Manoj123918 <seactivitycs@gmail.com>
2025-01-27 14:53:55 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,11 @@
+class sub
+{
+  public static void main(String args[])
+   {
+     int a,b,sub;
+     a=9;
+     b=4;
+     sub=a-b;
+System.out.println("Subtraction of 2 numbers is "+sub);
+  }
+}
